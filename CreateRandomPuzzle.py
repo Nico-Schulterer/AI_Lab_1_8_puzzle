3e67ac58443c7fb9b17635ca9d5c8eca763e9348
schwalmentin <schwalm@gmx.at>
2023-01-02 18:14:17 +0100

    Added nodes and functions to create and print Nodes

--- CreateRandomPuzzle.py
+++ CreateRandomPuzzle.py
@@ -1,4 +1,30 @@
-import numpy
+import numpy as np
 
+
+# A node class that contains the state of a puzzle field and its current cost
 class Node:
-    # int value = 0;
+    puzzleField = np.arange(9).reshape(3, 3)
+    g = 0
+    h = 0
+    f = 0
+
+
+# Returns a random shuffled node
+def createShuffledNode():
+    n1 = Node()
+    np.random.shuffle(n1.puzzleField.flat)
+    return n1
+
+
+# Prints the puzzle field of a node
+def printNode(node):
+    for x in range(3):
+        for y in range(3):
+            print("|", end=" ")
+            print(node.puzzleField[x][y], end=" ")
+
+        print("|")
+        if x < 2:
+            print("-------------")
+
+    print("")
